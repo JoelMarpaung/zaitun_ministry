dd24989493db9dd8a95254e2dcb6d20018aed7d9
Joel Marpaung <myitlife.jm@gmail.com>
2022-12-18 03:44:48 +0700

    adding circular backward to the image

--- radio/lib/presentation/pages/radio_page.dart
+++ radio/lib/presentation/pages/radio_page.dart
@@ -15,8 +15,7 @@ class RadioPage extends StatefulWidget {
   _RadioPageState createState() => _RadioPageState();
 }
 
-class _RadioPageState extends State<RadioPage>
-    with TickerProviderStateMixin {
+class _RadioPageState extends State<RadioPage> with TickerProviderStateMixin {
   late RadioPlayerBloc radioPlayerBloc;
   late AnimationController _animationController;
   late Animation<double> _animation;
@@ -30,11 +29,14 @@ class _RadioPageState extends State<RadioPage>
       duration: const Duration(seconds: 20),
     );
     _animation = Tween<double>(begin: 0, end: 1).animate(_animationController);
-    _animationControllers = List.generate( // Add this line
+    _animationControllers = List.generate(
+      // Add this line
       20, // Add this line
-          (index) => AnimationController( // Add this line
+      (index) => AnimationController(
+        // Add this line
         vsync: this, // Add this line
-        duration: Duration(milliseconds: Random().nextInt(1000) + 1000), // Add this line
+        duration: Duration(
+            milliseconds: Random().nextInt(1000) + 1000), // Add this line
       ), // Add this line
     ); // A
     super.initState();
@@ -43,7 +45,8 @@ class _RadioPageState extends State<RadioPage>
   @override
   void dispose() {
     _animationController.dispose();
-    for (final controller in _animationControllers) { // Add this line
+    for (final controller in _animationControllers) {
+      // Add this line
       controller.dispose(); // Add this line
     }
     super.dispose();
@@ -63,24 +66,53 @@ class _RadioPageState extends State<RadioPage>
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('Radio Zaitun', style: Theme.of(context).textTheme.headline2,),
-                      Text('Suara Kebenaran', style: Theme.of(context).textTheme.headline2,),
-                      const SizedBox(height: 10,),
-                      Text('105.8 FM Balige', style: Theme.of(context).textTheme.headline3,),
-                      const SizedBox(height: 10,),
-                      RotationTransition(
-                        turns: _animation,
-                        child: const Image(
-                          image: AssetImage('assets/icons/icon.png'),
-                          width: 200,
-                          height: 200,
-                        ),
+                      Text(
+                        'Radio Zaitun',
+                        style: Theme.of(context).textTheme.headline2,
                       ),
-                      const SizedBox(height: 10,),
+                      Text(
+                        'Suara Kebenaran',
+                        style: Theme.of(context).textTheme.headline2,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '105.8 FM Balige',
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Stack(
+                        children: [
+                          RotationTransition(
+                            turns: _animation,
+                            child: const Image(
+                              image: AssetImage('assets/icons/icon.png'),
+                              width: 200,
+                              height: 200,
+                            ),
+                          ),
+                          // Add the CustomPaint widget here
+                          CustomPaint(
+                            painter: _CirclePainter(
+                              animation:
+                                  _animation, // Use the same animation here
+                            ),
+                            size: const Size(200, 200),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
                       _buildRadioPlayer(state),
-                      const SizedBox(height: 10,),
+                      const SizedBox(
+                        height: 10,
+                      ),
                       SizedBox(
-                        height:100,
+                        height: 100,
                         child: Equalizer(
                           animationControllers: _animationControllers,
                           numBars: 20,
@@ -105,26 +137,32 @@ class _RadioPageState extends State<RadioPage>
     switch (state.runtimeType) {
       case RadioPlayerEmpty:
       case RadioPlayerStop:
-      _animationController.stop();
-      for (final controller in _animationControllers) { // Modify this line
-        controller.stop();
-      }
+        _animationController.stop();
+        for (final controller in _animationControllers) {
+          // Modify this line
+          controller.stop();
+        }
         return IconButton(
           highlightColor: Colors.transparent,
           splashColor: Colors.transparent,
           onPressed: () {
             _animationController.repeat();
-            for (final controller in _animationControllers) { // Modify this line
+            for (final controller in _animationControllers) {
+              // Modify this line
               controller.repeat(reverse: true);
             }
             radioPlayerBloc.add(const OnPlayRadio());
           },
           iconSize: 100,
-          icon: Icon(Icons.play_circle, color: Colors.indigo.shade900,),
+          icon: Icon(
+            Icons.play_circle,
+            color: Colors.indigo.shade900,
+          ),
         );
       case RadioPlayerPlay:
         _animationController.repeat();
-        for (final controller in _animationControllers) { // Modify this line
+        for (final controller in _animationControllers) {
+          // Modify this line
           controller.repeat(reverse: true);
         }
         return IconButton(
@@ -132,13 +170,17 @@ class _RadioPageState extends State<RadioPage>
           splashColor: Colors.transparent,
           onPressed: () {
             _animationController.stop();
-            for (final controller in _animationControllers) { // Modify this line
+            for (final controller in _animationControllers) {
+              // Modify this line
               controller.stop();
             }
             radioPlayerBloc.add(const OnStopRadio());
           },
           iconSize: 100,
-          icon: Icon(Icons.stop_circle, color: Colors.indigo.shade900,),
+          icon: Icon(
+            Icons.stop_circle,
+            color: Colors.indigo.shade900,
+          ),
         );
       case RadioPlayerLoading:
         return const CircularProgressIndicator();
@@ -156,5 +198,40 @@ class _RadioPageState extends State<RadioPage>
       default:
         return Container();
     }
+  }
+}
+
+class _CirclePainter extends CustomPainter {
+  _CirclePainter({
+    required this.animation,
+  }) : super(repaint: animation);
+
+  final Animation<double> animation;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue.shade800
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 8;
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = min(size.width, size.height) / 2;
+
+    final path = Path();
+    final startAngle = -animation.value * 2 * pi; // Use the negative value of the animation here
+    const arcAngle = 2 * pi;
+
+    // // Add a loop here
+    for (double i = startAngle; i < startAngle + arcAngle; i += pi / 10) {
+      path.moveTo(center.dx + radius * cos(i), center.dy + radius * sin(i));
+      path.lineTo(center.dx + radius * cos(i + pi / 5), center.dy + radius * sin(i + pi / 5));
+    }
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(_CirclePainter oldDelegate) {
+    return false;
   }
 }
